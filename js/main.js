b66bed3b533cd0fa2b28bc767d37ec6cec211295
Gabrieleba <73888700+Gabrieleba@users.noreply.github.com>
2020-11-11 15:48:21 +0200

    Intro to JavaScript

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// spausdiname sveikąjį skaičių
+console.log(98475);
+
+// du skaiciai vs desimtainis = kaip veikia ir koks yra kablelis
+console.log(3,14);
+console.log(3.14)
+
+// kaip spausdinamas tekstas?
+console.log("Labas rytas")
+console.log('Labas rytas')
